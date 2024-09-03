5136a655b495370148c16a58fe29bfa8a8b769f5
LeiWang1999 <leiwang1999@outlook.com>
2024-09-03 13:26:05 +0000

    simplify api

--- dataparallel.fp16.breakdown/thread_level_gemm_with_macro.py
+++ dataparallel.fp16.breakdown/thread_level_gemm_with_macro.py
@@ -5,26 +5,25 @@ from bitblas import tvm as tvm
 from tvm import tl as TL
 from bitblas.base.arch import CUDA
 from bitblas.utils import auto_detect_nvidia_target
-from bitblas.tl.utils import (
-    get_swizzle_layout,
-    mma_store_index_map,
-    get_ldmatrix_offset,
-)
+from bitblas.tl.utils import get_swizzle_layout
+from bitblas.tl.macro_generator import TensorCorePTXMacroGenerator
 
+in_dtype = "float16"
+accum_dtype = "float32"
 # Support we're from a config file
 arch = CUDA(auto_detect_nvidia_target())
 intrin_info = bitblas.base.hint.IntrinInfo(
-    in_dtype="float16",
-    out_dtype="float32",
+    in_dtype=in_dtype,
+    out_dtype=accum_dtype,
     trans_b=True,
 )
 config = bitblas.base.Hint.from_dict(
     {
         "arch": arch,
-        "block": [64, 64],
-        "warp": [32, 32],
+        "block": [128, 256],
+        "warp": [64, 64],
         "rstep": [32],
-        "pipeline_stage": 1,
+        "pipeline_stage": 2,
         "use_async": False,
         "intrin_info": intrin_info,
         "shared_scope": "shared.dyn",
@@ -44,13 +43,13 @@ chunk = config.rstep[0]
 shared_scope = config.shared_scope
 micro_size_x, micro_size_y, micro_size_k = 16, 16, 16
 
-M = 256
-N = 256
-K = 256
+M = 16384
+N = 16384
+K = 16384
 
-A = torch.rand(M, K, device="cuda", dtype=torch.float16)
-B = torch.rand(N, K, device="cuda", dtype=torch.float16)
-C = torch.zeros(M, N, device="cuda", dtype=torch.float32)
+A = torch.rand(M, K, device="cuda", dtype=getattr(torch, in_dtype))
+B = torch.rand(N, K, device="cuda", dtype=getattr(torch, in_dtype))
+C = torch.zeros(M, N, device="cuda", dtype=getattr(torch, accum_dtype))
 
 
 import tvm.tl.language as T
@@ -92,6 +91,12 @@ def tl_matmul(
     warp_rows = warp_row_tiles // micro_size_x
     warp_cols = warp_col_tiles // micro_size_y
 
+    ptx_macro_generator = TensorCorePTXMacroGenerator(
+        a_dtype=dtypeAB, b_dtype=dtypeAB, accum_dtype=accum_dtype,
+        a_transposed=False, b_transposed=True, block_row_warps=block_row_warps,
+        block_col_warps=block_col_warps, warp_row_tiles=warp_row_tiles,
+        warp_col_tiles=warp_col_tiles, chunk=chunk, threads=threads
+    )
     @T.prim_func
     def main(
         A: T.Buffer(A_shape, dtypeAB),
@@ -109,22 +114,12 @@ def tl_matmul(
             B_local = T.alloc_fragment((warp_cols * local_size), dtypeAB, scope="local")
             C_local = T.alloc_fragment((warp_rows * warp_cols * local_size), accum_dtype, scope="local")
             thread_bindings = T.thread_binding(0, threads, "threadIdx.x")
-            tx = thread_bindings % warp_size
-            ty = (thread_bindings // warp_size) % block_row_warps
-            tz = thread_bindings // (warp_size * block_row_warps)
-
-            T.annotate_layout(
-                {
-                    A_shared: make_swizzle_layout(A_shared),
-                    B_shared: make_swizzle_layout(B_shared),
-                }
-            )
 
             for i in T.serial(warp_rows * warp_cols * local_size):
                 C_local[i] = 0
 
             for ko in T.Pipelined((K // block_K), num_stages=(stage - 1)):
-                # TODO(lei): storage sync should be able to be injected automatically by TVM Pass
+                # TODO(lei): storage sync should be injected automatically by TVM Pass
                 T.tvm_storage_sync("shared")
 
                 # Load A into shared memory
@@ -135,35 +130,41 @@ def tl_matmul(
                 for j, k in T.Parallel(block_N, block_K):
                     B_shared[j, k] = B[bx * block_N + j, ko * block_K + k]
 
-                # TODO(lei): storage sync should be able to be injected automatically by TVM Pass
+                # TODO(lei): storage sync should be injected automatically by TVM Pass
                 T.tvm_storage_sync("shared")
 
                 for ki in T.serial(0, (block_K // micro_size_k)):
                     # Load A into fragment
-                    for i in T.serial(warp_rows):
-                        T.ptx_ldmatrix("float16", T.bool(False), 4, ".b16", A_local.data, i * local_size, 
-                            T.address_of(A_shared[ty * warp_row_tiles + i * micro_size_x, ki * micro_size_k]), get_ldmatrix_offset("A", tx, 0, block_K, "float16", False))
-
-                    # Load B into fragment
-                    for j in T.serial(warp_cols):
-                        T.ptx_ldmatrix("float16", T.bool(False), 4, ".b16", B_local.data, j * local_size,
-                            T.address_of(B_shared[tz * warp_col_tiles + j * micro_size_y, ki * micro_size_k]), get_ldmatrix_offset("B", tx, 0, block_K, "float16", True))
-
-                    # Apply MMA
-                    for i, j in T.grid(warp_rows, warp_cols,):
-                        T.ptx_mma("float32", "m16n8k16", "row", "col", "fp16", "fp16", "fp32", A_local.data, i * local_size, B_local.data, j * local_size, C_local.data, i * warp_cols * local_size + j * local_size, T.bool(False),)
-                        T.ptx_mma("float32", "m16n8k16", "row", "col", "fp16", "fp16", "fp32", A_local.data, i * local_size, B_local.data, j * local_size + 4, C_local.data, i * warp_cols * local_size + j * local_size + 4, T.bool(False))
-
-            # STS
-            # MMA Store must be in simulated instead of TVM Intrins
-            # As TVM Intrins is like a hack that the threadIdx.x should be always
-            # equal to the warp_size
-            for i, j in T.grid(warp_rows, warp_cols):
-                for local_id in T.serial(local_size):
-                    row, col = T.meta_var(
-                        mma_store_index_map(tx, local_id)
+                    ptx_macro_generator.LDMATRIX_A(
+                        ptx_macro_generator,
+                        A_local,
+                        A_shared,
+                        ki,
+                        thread_bindings=thread_bindings,
                     )
-                    C_shared[ty * warp_rows + i, tz * warp_cols + j, row, col] = C_local[i * (warp_cols * local_size) + j * local_size + local_id]
+                    
+                    # Load B into fragment
+                    ptx_macro_generator.LDMATRIX_B(
+                        ptx_macro_generator,
+                        B_local,
+                        B_shared,
+                        ki,
+                        thread_bindings=thread_bindings,
+                    )
+
+                    ptx_macro_generator.MMA(
+                        ptx_macro_generator,
+                        A_local,
+                        B_local,
+                        C_local
+                    )
+
+            ptx_macro_generator.STMATRIX(
+                ptx_macro_generator,
+                C_local,
+                C_shared,
+                thread_bindings=thread_bindings,
+            )
 
             for i, j in T.Parallel(block_M, block_N):
                 C[by * block_M + i, bx * block_N + j] = C_shared[i // micro_size_x, j // micro_size_y, i % micro_size_x, j % micro_size_y]
